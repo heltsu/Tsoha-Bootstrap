a1ca60da51ab1719ed60b7ce661acf73a59d0a53
Heli <hniemima@helsinki.fi>
2015-08-02 20:30:55 +0300

    järjestelmän tietosisältö ja relaatiotietokantakaavio sekä taulut

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,1 +1,3 @@
 -- Lisää INSERT INTO lauseet tähän tiedostoon
+INSERT INTO Member (name, password) VALUES ('Tytti', 'Tytti123');
+INSERT INTO Member (name, password) VALUES ('Totti', 'Totti123');
--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,7 @@
 -- Lisää CREATE TABLE lauseet tähän tiedostoon
+CREATE TABLE Member(
+id SERIAL PRIMARY KEY,
+name varchar(20) NOT NULL,
+password varchar(16) NOT NULL
+);
+
