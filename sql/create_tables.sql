82b7bf2001ba7e7ae229115e24729cfaae547f08
Heli <hniemima@helsinki.fi>
2015-08-03 21:46:46 +0300

    taulut lisätty

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -5,3 +5,16 @@ name varchar(20) NOT NULL,
 password varchar(16) NOT NULL
 );
 
+CREATE TABLE Job(
+id SERIAL PRIMARY KEY,
+member_id INTEGER REFERENCES Member(id),
+name varchar NOT NULL,
+priority varchar(2) NOT NULL,
+added DATE,
+done DATE
+);
+
+CREATE TABLE Category(
+id SERIAL PRIMARY KEY,
+name varchar(20) NOT NULL
+);
