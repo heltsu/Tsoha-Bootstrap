630f1ee28f9e9c97241097b72a41da26020cf8ba
Heli <hniemima@helsinki.fi>
2015-08-10 21:07:52 +0300

    viikko3

--- app/controllers/askare_controller.php
+++ app/controllers/askare_controller.php
@@ -1,17 +1,37 @@
 <?php
 
 //require 'app/models/askare.php';
-class AskareController extends BaseController{
-    
-    public static function index(){
-       $askareet = Askare::all();
-       View::make('askare/index.html', array('askareet' =>$askareet));
+class AskareController extends BaseController {
+
+    public static function index() {
+        $askareet = Askare::all();
+        View::make('askare/index.html', array('askareet' => $askareet));
     }
-    
-    public static function show($id){
+
+    public static function show($id) {
         $askare = Askare::find($id);
-        View::make('askare/show.html', array('askare'=>$askare));
+        View::make('askare/show.html', array('askare' => $askare));
     }
+    
+    public static function create(){
+        View::make('askare/new.html');
+    }
+
+    public static function store() {
+        $params = $_POST;
+        
+        $askare = new Askare(array(
+            'nimi' => $params['nimi'],
+            'tarkeys' => $params['tarkeys'],
+            'lisatty' => $params['lisatty'],
+            'valmis' => $params['valmis']
+                ));
+
+    //    Kint::dump($params);
+        
+        $askare->save();
+
+     //   Redirect::to('/askare/' . $askare->id, array('message' => 'Askare lisätty'));
+    }
+
 }
-
-
--- config/routes.php
+++ config/routes.php
@@ -12,22 +12,22 @@ $routes->get('/askare', function() {
     AskareController::index();
 });
 
-$routes->get('/askare/:id', function($id){
+$routes->post('/askare', function() {
+    AskareController::store();
+});
+
+$routes->get('/askare/new', function() {
+    AskareController::create();
+});
+
+$routes->get('/askare/:id', function($id) {
     AskareController::show($id);
 });
 
-$routes->get('/askare/2', function(){
-HelloWorldController::askareet_muokkaus();
+$routes->get('/askare/2', function() {
+    HelloWorldController::askareet_muokkaus();
 });
 
-$routes->get('/login', function(){
-HelloWorldController::askareet_login();
-});
-
-$routes->post('/askare', function(){
-AskareController::store();
-});
-
-$routes->get('/askare/new', function(){
-AskareController::create();
+$routes->get('/login', function() {
+    HelloWorldController::askareet_login();
 });
